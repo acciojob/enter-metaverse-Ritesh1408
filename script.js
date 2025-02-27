8c3fafb49a20b379526d0b358ebfb64af8db9824
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-02-27 23:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let text = document.getElementById("status")
+let btn = document.getElementById("enterBtn")
+
+btn.onclick = function () {
+    text.innerHTML = "<h1>Entered Metaverse</h1>";
+}
